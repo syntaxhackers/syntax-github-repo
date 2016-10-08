033ff602b28f0df0b7b0f3feb4031945b31fd060
Syntax <syntaxhackers@gmail.com>
2016-10-08 15:03:01 +0100

    Build 9
     + Added new terminal commands

--- static/js/focus.js
+++ static/js/focus.js
@@ -7,7 +7,7 @@ function focusToInput()
 	command.focus();
 	/*setTimeout(focusToInput, 1000);*/
 	autoFocusToInput();
-	log("You have connceted to the syntax terminal!\nType <not added yet> for a list of commands.\nThe two commands at the moment are 'echo <message..>' and 'goto_cxx_group'!\n'goto_cxx_group' is deprecated.\n\nImportant:\n - Underscores in the input box look invisible\n - The underscores are still there though!\n")
+	log("You have connceted to the syntax terminal!\nType 'help' for a list of commands.\n")
 }
 
 function autoFocusToInput()
@@ -64,9 +64,52 @@ function handleEchoCommand(args)
 	log(args.join(" "));
 }
 
-function handleGoto_cxx_groupCommand(args)
+function handleHelpCommand(args)
 {
-	window.location = "/group?cnael=CXX";
+	log("-------------HELP--------------")
+	log("Help:")
+	log("  Usage: help")
+	log("  Description: Shows help for all commands")
+	log("Echo:")
+	log("  Usage: echo [message..]")
+	log("  Description: Sends a message to the console")
+	log("Cls:")
+	log("  Alias: clear")
+	log("  Usage: cls")
+	log("  Description: Clears the console")
+	log("Lsg:")
+	log("  Usage: lsg")
+	log("  Description: Lists all groups you have access to.")
+	log("Cg:")
+	log("  Usage: cg <group>")
+	log("  Description: Changes current group to <group>")
+	log("-------------------------------")
+}
+
+function handleClearCommand(args)
+{
+	cons.innerHTML = "";
+}
+
+function handleClsCommand(args)
+{
+	handleClearCommand(args);
+}
+
+function handleLsgCommand(args)
+{
+	log("------------GROUPS-------------")
+	log("You have access to:")
+	log(" - CXX")
+	log("-------------------------------")
+}
+
+function handleCgCommand(args)
+{
+	if (equalsIgnoreCase(args[0], "cxx"))
+		window.location = "/group?cnael=CXX";
+	else
+		log("Error: Invalid group");
 }
 
 function log(msg)
@@ -86,4 +129,20 @@ function log(msg)
 function capitalizeCase(str)
 {
 	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function equalsIgnoreCase(str, str2)
+{
+	try
+	{
+		if (str.toLowerCase() == str2.toLowerCase())
+		{
+			return true;
+		}
+		return false;
+	}
+	catch(e)
+	{
+		return false;
+	}
 }
