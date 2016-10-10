58bac454b1106710047cdb5ac5422e8ad0ba737c
Syntax <syntaxhackers@gmail.com>
2016-10-10 21:37:37 +0100

    Build 17
     + Added encryption command to terminal

--- static/js/CommandManager.js
+++ static/js/CommandManager.js
@@ -100,22 +100,25 @@ CommandManager.prototype.executeClearCommand = function()
 CommandManager.prototype.executeHelpCommand = function(args)
 {
 	this.console.log("-------------HELP--------------")
-	this.console.log("Help:")
+	this.console.log("HELP:")
 	this.console.log("  Usage: help")
 	this.console.log("  Description: Shows help for all commands")
-	this.console.log("Echo:")
+	this.console.log("ECHO:")
 	this.console.log("  Usage: echo [message..]")
 	this.console.log("  Description: Sends a message to the console")
-	this.console.log("Cls:")
+	this.console.log("CLS:")
 	this.console.log("  Alias: clear")
 	this.console.log("  Usage: cls")
 	this.console.log("  Description: Clears the console")
-	this.console.log("Lsg:")
+	this.console.log("LSG:")
 	this.console.log("  Usage: lsg")
 	this.console.log("  Description: Lists all groups you have access to.")
-	this.console.log("Cg:")
+	this.console.log("CG:")
 	this.console.log("  Usage: cg <group>")
 	this.console.log("  Description: Changes current group to <group>")
+	this.console.log("ED:")
+	this.console.log("  Usage: 'ed my message -e' or 'ed my message -d'")
+	this.console.log("  Description: Encrypts/Decrypts a message")
 	this.console.log("-------------------------------")
 }
 
@@ -142,4 +145,26 @@ CommandManager.prototype.executeCgCommand = function(args)
 CommandManager.prototype.executeSgCommand = function()
 {
 	this.console.log("The selected group is: " + this.data['group'])
+}
+
+CommandManager.prototype.executeEdCommand = function(args, ops, kwops)
+{
+	choice = kwops['-m'];
+	result = "";
+
+	if (choice == 'e')
+	{
+		//encrypt
+		result = getEDEncrypted(args.join(" "))
+	}
+	else if (choice == 'd')
+	{
+		//decrypt
+		result = getEDDecrypted(args.join(" "))
+	}
+	else
+	{
+		result = "Error; Please add '-m e' (to encrypt) or '-m d' (to decrypt) to the end of your line"
+	}
+	this.console.log("ED result: " + result);
 }
